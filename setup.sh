8dca04b2c9dfb20bad4b7e33fd393810082174a7
Andrew Bloese <dev@andrewbloese.me>
2023-12-03 05:12:39 +0000

    initial commit

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# get latest and upgrades for all packages
+sudo apt-get update -y && sudo apt-get upgrade -y
+
+#install npm an 'n' as a version manager
+sudo apt-get install npm -y
+
+#install n and set to lts (20.10.0) version as of December 2, 2023
+n install 20.10.0
+n use 20.10.0
+
